a58db18109670b9503c7e35ba47968144d17019b
Gabriele Maddalena <gabriele.madd@gmail.com>
2016-05-30 22:46:06 +0200

    added brand selection for all products for all taxonomies

--- app/Http/Controllers/TaxonController.php
+++ app/Http/Controllers/TaxonController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Taxon;
+
+use Illuminate\Support\Facades\Input;
+
 class TaxonController extends Controller
 {
     /**
@@ -18,7 +22,14 @@ class TaxonController extends Controller
 
     }
 
-
+    public function debug_to_console($data) {
+        if(is_array($data) || is_object($data))
+        {
+            echo("<script>console.log('PHP: ".json_encode($data)."');</script>");
+        } else {
+            echo("<script>console.log('PHP: ".$data."');</script>");
+        }
+    }
     /**
      * Display the specified resource.
      *
@@ -27,11 +38,22 @@ class TaxonController extends Controller
      */
     public function show($id)
     {
-        $taxon = \App\Taxon::find($id);
-        if($taxon){
-            $devices = $taxon->devices();
+        $taxon = Taxon::find($id);
+        $compId = Input::get('company');
+        $this->debug_to_console($compId);
+        if(isset($compId)){
+            if($taxon){
+                $this->debug_to_console('ENTRATO');
+                $devices = $taxon->devices()->where('company_id', $compId)->get();
+                return view('taxon.show', ['taxon' => $taxon, 'devices' => $devices]);
+            }
         }
-        return view('taxon.show', ['taxon' => $taxon, 'devices' => $devices]);
+        else{
+            if($taxon){
+                $devices = $taxon->devices()->get();
+                return view('taxon.show', ['taxon' => $taxon, 'devices' => $devices]);
+            }
+        }
     }
 
   }
