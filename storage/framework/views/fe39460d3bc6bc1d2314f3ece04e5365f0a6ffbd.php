e79afb2918a3c86d16ecd2ca10b5b4dce7de400c
Georgy Shabunin <georgy.shabunin@gmail.com>
2016-07-01 02:17:25 +0200

    dropdown responsive

--- storage/framework/views/fe39460d3bc6bc1d2314f3ece04e5365f0a6ffbd.php
+++ storage/framework/views/fe39460d3bc6bc1d2314f3ece04e5365f0a6ffbd.php
@@ -1,15 +1,17 @@
-<h2><?php echo e($taxon->name); ?></h2>
-<ul class="list-group">
-<?php foreach($assistanceServices as $assistanceService): ?>
+<div class="container">
+  <h2><?php echo e($taxon->name); ?></h2>
+  <ul class="list-group">
+  <?php foreach($assistanceServices as $assistanceService): ?>
 
-  <li class="list-group-item">
-    <?php if($taxon->most_popular): ?>
-      <span class="label label-default label-pill pull-right"><?php echo e($assistanceService->views()->count()); ?></span>
-    <?php endif; ?>
+    <li class="list-group-item">
+      <?php if($taxon->most_popular): ?>
+        <span class="label label-default label-pill pull-right"><?php echo e($assistanceService->views()->count()); ?></span>
+      <?php endif; ?>
 
-    <?php echo HTML::linkAction('AssistanceServiceController@show', $assistanceService->name,  $assistanceService->id, []); ?>
+      <?php echo HTML::linkAction('AssistanceServiceController@show', $assistanceService->name,  $assistanceService->id, []); ?>
 
-  </li>
+    </li>
 
-<?php endforeach; ?>
-</ul>
+  <?php endforeach; ?>
+  </ul>
+</div>
